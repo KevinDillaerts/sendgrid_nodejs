e4c0501ee13aac57983dcebf8075b5c4b5fd486a
KevinDillaerts <69395404+KevinDillaerts@users.noreply.github.com>
2024-12-09 12:18:46 +0100

    updated npm packages

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const indexRouter = require("./routes/api/index");
-
-const port = process.env.PORT || 4000;
+const apiRouter = require("./routes/api/index"); // Import your routes
 
 const app = express();
 
+// CORS Configuration
 const corsOptions = {
-  // TODO: change to domain website
-  origin: "*",
-  methods: "POST, GET",
-  optionsSuccessStatus: 200,
+  origin: "https://sigridvolders.be", // Allow only your frontend domain
+  methods: "GET,POST,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api", indexRouter);
 
-app.listen(port, () => console.log(`The server is running on port ${port}`));
+// Attach routes to the app
+app.use("/api", apiRouter);
 
-module.exports = express;
+const port = process.env.PORT || 4000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,16 @@
-require("dotenv").config();
 const express = require("express");
 const sgMail = require("@sendgrid/mail");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.status(200).json({ bericht: "test" });
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, world!" });
 });
 
 router.post("/", async (req, res) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   const data = req.body;
-  console.log(req.body);
   const msg = {
     from: process.env.SENDGRID_FROM_EMAIL,
     template_id: process.env.SENDGRID_TEMPLATE_ID,
@@ -29,10 +28,10 @@ router.post("/", async (req, res) => {
       },
     ],
   };
-  JSON.stringify(msg.personalizations);
+
   try {
     await sgMail.send(msg);
-    res.status(201).json({ message: "Email succesfully send!" });
+    res.status(201).json({ message: "Email successfully sent!" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Failed to send the email." });
